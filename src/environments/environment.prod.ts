eb8d884613bc49e24f63df4812a48aa0e8796e83
Mostafa Khalil <102040483+Mostafakhalil21@users.noreply.github.com>
2023-12-27 10:27:58 +0200

    prod-enviroment apiurl change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'http://18.153.218.206:3000',
+    apiUrl: 'http://api.xiteit.co',
     apiKey: 'Token 5pp-025696b08543d72bdcd4',
   };
   
